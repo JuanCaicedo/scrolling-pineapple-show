d2ded267c4bd0624de875e782aac13adccf1a5f9
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-09-26 08:34:01 -0600

    spinning pineapple

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,89 @@
-import Image from 'next/image'
+"use client";
+import { Animation, Pin, Root, Waypoint } from "@bsmnt/scrollytelling";
+import Image from "next/image";
+import { useState } from "react";
+import styles from "./page.module.css";
+
+const totalFrames = 8;
+
+const frames = (frame: number): number => frame * (100 / totalFrames);
+
+function SpinningPineapple({
+  startFrame,
+  endFrame,
+  image,
+  startShowing = false,
+}: {
+  startFrame: number;
+  endFrame: number;
+  image: number;
+  startShowing: boolean;
+}) {
+  const [show, setShow] = useState(startShowing);
+
+  return (
+    <>
+      <Waypoint
+        at={frames(startFrame)}
+        onCall={() => {
+          setShow(true);
+        }}
+        disabled={false}
+      />
+      {show && (
+        <Image
+          src={`/spin-${image}.png`}
+          alt="Spinning pineapple"
+          width={300}
+          height={240}
+          priority
+        />
+      )}
+      <Waypoint
+        at={frames(endFrame)}
+        onCall={() => setShow(false)}
+        disabled={false}
+      />
+    </>
+  );
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="">
+      <Root>
+        <div>
+          <Pin childHeight={"100vh"} pinSpacerHeight={"300vh"} top={0}>
+            <div className={styles.frame1}>
+              <SpinningPineapple
+                startFrame={0}
+                endFrame={1}
+                image={1}
+                startShowing
+              />
+              <SpinningPineapple startFrame={1} endFrame={2} image={2} />
+              <SpinningPineapple startFrame={2} endFrame={3} image={3} />
+              <SpinningPineapple startFrame={3} endFrame={4} image={4} />
+              <SpinningPineapple startFrame={4} endFrame={5} image={5} />
+              <SpinningPineapple startFrame={5} endFrame={6} image={6} />
+              <SpinningPineapple startFrame={6} endFrame={7} image={7} />
+              <SpinningPineapple startFrame={7} endFrame={8} image={8} />
+            </div>
+          </Pin>
+          <Pin childHeight={10} pinSpacerHeight={"100vh"} top={0}>
+            <div className={styles.frame2}>
+              <SpinningPineapple startFrame={0} endFrame={1} image={1} />
+              <SpinningPineapple startFrame={1} endFrame={2} image={2} />
+              <SpinningPineapple startFrame={2} endFrame={3} image={3} />
+              <SpinningPineapple startFrame={3} endFrame={4} image={4} />
+              <SpinningPineapple startFrame={4} endFrame={5} image={5} />
+              <SpinningPineapple startFrame={5} endFrame={6} image={6} />
+              <SpinningPineapple startFrame={6} endFrame={7} image={7} />
+              <SpinningPineapple startFrame={7} endFrame={8} image={8} />
+            </div>
+          </Pin>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </Root>
     </main>
-  )
+  );
 }
