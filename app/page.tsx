f81c675a7a82fdbc51158de6a01801b55c334432
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-09-27 18:27:43 -0600

    debug pineapple spin

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { Animation, Pin, Root, Waypoint } from "@bsmnt/scrollytelling";
-import { v4 as uuidv4 } from "uuid";
 import Image from "next/image";
 import { useState } from "react";
 import styles from "./page.module.css";
+import Head from "next/head";
 
-const totalFrames = 9;
+const totalFrames = 18;
+const totalHeight = 3600;
 
 const frames = (frame: number): number => frame * (100 / totalFrames);
 
@@ -18,41 +19,65 @@ function SpinningPineapple({ startFrame }: { startFrame: number }) {
 
   return (
     <>
-      {pineappleFrames.map((f) => (
-        <Waypoint
-          key={`pineapple-frame-${f}`}
-          at={frames(f)}
-          onCall={() => {
-            setSrc(spinSrc(f));
-          }}
-          onReverseCall={() => {
-            setSrc(spinSrc(f));
-          }}
-          disabled={false}
+      {pineappleFrames.map((f) => {
+        const at = frames(f + startFrame);
+        return (
+          <Waypoint
+            key={`pineapple-frame-${f}`}
+            at={at}
+            onCall={() => {
+              setSrc(spinSrc(f));
+            }}
+            onReverseCall={() => {
+              setSrc(spinSrc(f));
+            }}
+            disabled={false}
+          />
+        );
+      })}
+      <div className={styles.container}>
+        <Image
+          src={src}
+          alt="Spinning pineapple"
+          fill={true}
+          className={styles.image}
+          priority
         />
-      ))}
-      <Image
-        src={src}
-        alt="Spinning pineapple"
-        width={300}
-        height={240}
-        priority
-      />
+      </div>
     </>
+  );
+}
+
+function PreloadImages() {
+  return (
+    <Head>
+      {pineappleFrames.map((f) => (
+        <link rel="preload" href={spinSrc(f)} as="image" />
+      ))}
+    </Head>
   );
 }
 
 export default function Home() {
   return (
     <main className="">
+      <PreloadImages/>
       <Root>
         <div>
-          <Pin childHeight={"0vh"} pinSpacerHeight={"2200vh"} top={0}>
+          <Pin
+            childHeight={"0vh"}
+            pinSpacerHeight={`${totalHeight / 2}vh`}
+            top={0}
+          >
             <div className={styles.frame1}>
               <SpinningPineapple startFrame={0} />
             </div>
           </Pin>
-          <Pin childHeight={"100vh"} pinSpacerHeight={"100vh"} top={0}>
+          <Pin
+            childHeight={"100vh"}
+            pinSpacerHeight={`${totalHeight / 2}vh`}
+            top={0}
+          >
             <div className={styles.frame2}>
               <SpinningPineapple startFrame={9} />
             </div>
