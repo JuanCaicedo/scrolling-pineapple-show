706e5bcfc5ca5837fc3fba9efd68d96608fd2d55
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-10-08 22:34:57 -0600

    move intro scene

--- app/intro/page.tsx
+++ app/intro/page.tsx
@@ -1,83 +1,10 @@
-// @ts-nocheck
 "use client";
-import {
-  Pin,
-  Animation,
-  Parallax,
-  Root,
-  Waypoint,
-  ImageSequenceCanvas,
-} from "@bsmnt/scrollytelling";
-import Image from "next/image";
-import { useEffect, useState, useRef, ReactNode } from "react";
-import styles from "./page.module.css";
-import Head from "next/head";
 import IntroScene from "@/scenes/IntroScene";
-
-const totalFrames = 9;
-const totalHeight = 3600;
-
-const frames = (frame: number): number => frame * (100 / totalFrames);
-
-const pineappleFrames = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-const spinSrc = (frame: number) => `/spin-${frame}.png`;
-
-function PreloadImages() {
-  return (
-    <Head>
-      {[].map((f) => (
-        <link rel="preload" href={spinSrc(f)} as="image" key={`preload-${f}`} />
-      ))}
-    </Head>
-  );
-}
 
 export default function Home() {
   return (
     <main className="">
-      <Root>
-        <Pin childHeight={"100vh"} pinSpacerHeight={`800vh`}>
-          <div className={styles.panel}>
-            <Animation
-              tween={{
-                start: 0,
-                end: 10,
-                from: { top: "-120vh", right: 0, ease: "linear" },
-              }}
-            >
-              <div className={`${styles.image} ${styles.spotlight}`} />
-            </Animation>
-            <Animation
-              tween={{
-                start: 10,
-                end: 40,
-                from: { top: "110vh", ease: "linear" },
-              }}
-            >
-              <img
-                alt="Pinapple no sunglasses"
-                src={"/no-sunglasses.png"}
-                className={`${styles.image} ${styles.pineappleNoSunglasses}`}
-              />
-            </Animation>
-
-            <Animation
-              tween={{
-                start: 40,
-                end: 100,
-                from: { top: "45vh", ease: "linear" },
-              }}
-            >
-              <img
-                alt="Sunglasses"
-                src={"/sunglasses.png"}
-                className={`${styles.image} ${styles.sunglasses}`}
-              />
-            </Animation>
-          </div>
-        </Pin>
-      </Root>
+      <IntroScene />
     </main>
   );
 }
--- scenes/IntroScene.tsx
+++ scenes/IntroScene.tsx
@@ -1,34 +1,51 @@
-import { Pin, Animation } from "@bsmnt/scrollytelling";
-import Image from "next/image";
+import { Pin, Animation, Root } from "@bsmnt/scrollytelling";
 import styles from "./page.module.css";
 
 const totalHeight = 3600;
 
 export default function IntroScene() {
   return (
-    <Pin childHeight={"100vh"} pinSpacerHeight={`800vh`} top={100}>
-      <Animation
-        tween={{
-          from: {
-            top: 0,
-          },
-          to: {
-            top: -40,
-            ease: "linear",
-          },
+    <Root>
+      <Pin childHeight={"100vh"} pinSpacerHeight={`800vh`}>
+        <div className={styles.panel}>
+          <Animation
+            tween={{
+              start: 0,
+              end: 10,
+              from: { top: "-120vh", right: 0, ease: "linear" },
+            }}
+          >
+            <div className={`${styles.image} ${styles.spotlight}`} />
+          </Animation>
+          <Animation
+            tween={{
+              start: 10,
+              end: 40,
+              from: { top: "110vh", ease: "linear" },
+            }}
+          >
+            <img
+              alt="Pinapple no sunglasses"
+              src={"/no-sunglasses.png"}
+              className={`${styles.image} ${styles.pineappleNoSunglasses}`}
+            />
+          </Animation>
 
-          start: 0,
-          end: 100,
-        }}
-      >
-        <Image
-          alt="Pineapple"
-          src={"/spin-1.png"}
-          width={100}
-          height={100}
-          className={`${styles.image} ${styles.pineapple}`}
-        />
-      </Animation>
-    </Pin>
+          <Animation
+            tween={{
+              start: 40,
+              end: 100,
+              from: { top: "45vh", ease: "linear" },
+            }}
+          >
+            <img
+              alt="Sunglasses"
+              src={"/sunglasses.png"}
+              className={`${styles.image} ${styles.sunglasses}`}
+            />
+          </Animation>
+        </div>
+      </Pin>
+    </Root>
   );
 }
