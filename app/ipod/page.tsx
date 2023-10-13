d9285df1e4bb14e58b04ec8768c104ff088e26f6
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-10-13 09:45:58 -0600

    plug in ipod

--- app/ipod/page.tsx
+++ app/ipod/page.tsx
@@ -1,22 +1,37 @@
 "use client";
-import { Pin, Root, Waypoint } from "@bsmnt/scrollytelling";
+import { Animation, Pin, Root, Waypoint } from "@bsmnt/scrollytelling";
 import Image from "next/image";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import styles from "./page.module.css";
 import Head from "next/head";
+import { getStaggeredTimeline } from "./getStaggeredTimeline";
 
-const totalFrames = 16;
+const totalFrames = 17;
 const totalHeight = 3600;
 
 const frames = (frame: number): number => frame * (100 / totalFrames);
 
-const ipodFrames = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-// const ipodFrames = [1,2]
+const ipodFrames = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-const spinSrc = (frame: number) => `/ipod-${frame}.png`;
+const spinSrc = (frame: number): string => {
+  if (frame === 17) {
+    return `/ipod-1.png`;
+  }
+  return `/ipod-${frame}.png`;
+};
 
 function SpinningIpod() {
   const [src, setSrc] = useState(spinSrc(ipodFrames[0]));
+  const ref = useRef<HTMLImageElement>(null);
+  const plugRef = useRef<HTMLImageElement>(null);
+
+  const spinTimeline = getStaggeredTimeline({
+    start: 0,
+    end: 60,
+    chunks: ipodFrames.length,
+    overlap: 0,
+  });
+  const lastFrame = spinTimeline[spinTimeline.length - 1];
 
   return (
     <>
@@ -36,15 +51,38 @@ function SpinningIpod() {
           />
         );
       })}
+      <Animation
+        tween={{
+          target: ref,
+          start: 0,
+          end: lastFrame.end,
+          from: { top: "-100%" },
+        }}
+      />
       <div className={styles.container}>
         <Image
+          ref={ref}
           src={src}
           alt="Spinning ipod"
           fill={true}
-          className={"image"}
+          className={`image ${styles.ipod}`}
           priority
         />
       </div>
+      <Animation
+        tween={{
+          target: plugRef,
+          start: lastFrame.end,
+          end: 80,
+          from: { top: "-100%" },
+        }}
+      />
+      <img
+        ref={plugRef}
+        src={"/plug.png"}
+        alt="Aux plug"
+        className={`image ${styles.plug}`}
+      />
     </>
   );
 }
