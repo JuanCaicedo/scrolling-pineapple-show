66c1bb1baf8a982777b4f9be6bd520c96873795b
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-10-16 09:02:38 -0600

    scaffold out tunnel

---
+++ app/out-tunnel/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Pin, Root, Animation } from "@bsmnt/scrollytelling";
+import PineappleRunThreeQuarter from "@/components/PineappleRunThreeQuarter";
+import { useRef } from "react";
+import styles from "./page.module.css";
+
+export default function PineappleRunPage() {
+  const togetherRef = useRef<HTMLImageElement>(null);
+  return (
+    <Root start="top top" end="bottom bottom" scrub={2}>
+      <Pin childHeight={"100vh"} pinSpacerHeight={`800vh`}>
+        <div className={"panel"}>
+          <Animation
+            tween={{
+              start: 0,
+              end: 100,
+              target: togetherRef,
+              from: { left: "-130%" },
+            }}
+          />
+          <div className={styles["run-container"]} ref={togetherRef}>
+            <div className={styles["together-container"]}>
+              <PineappleRunThreeQuarter className={styles.pineapple} />
+            </div>
+          </div>
+        </div>
+      </Pin>
+    </Root>
+  );
+}
