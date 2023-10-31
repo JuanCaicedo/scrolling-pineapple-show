51e54c7fc3d006f5c49bf617ad4e9b2eb9c32f33
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-10-31 09:18:09 -0600

    more fade in

--- scenes/DanceScene/index.tsx
+++ scenes/DanceScene/index.tsx
@@ -14,55 +14,26 @@ import {
 } from "@/app/utils/ImageSequence";
 import { getStaggeredTimeline } from "@/app/utils/getStaggeredTimeline";
 
-function FadeInAndOut({
-  start,
-  end,
-  name,
-}: {
-  start: number;
-  end: number;
-  name: string;
-}) {
-  const ref = useRef<HTMLImageElement>(null);
-  const startFrame1 = start;
-  const third = (end - start) / 3;
-  const endFrame1 = startFrame1 + third;
-  const startFrame2 = endFrame1 + 1;
-  const endFrame2 = end;
-  const className = `dancing-${name}`;
-  return (
-    <>
-      <img
-        ref={ref}
-        src={`/dance-${name}.png`}
-        alt={`Dancing ${name}`}
-        className={`${styles[className]} image-fixed ${styles["color-highlight"]}`}
-      />
-      <img
-        src={`/dark-${name}.png`}
-        alt={`Dark dancing ${name}`}
-        className={`${styles[className]} image-fixed`}
-      />
-
-      <Animation
-        tween={{
-          start: startFrame1,
-          end: endFrame1,
-          target: ref,
-          fromTo: [{ opacity: 0 }, { opacity: 1, ease: "none" }],
-        }}
-      />
-      <Animation
-        tween={{
-          start: startFrame2,
-          end: endFrame2,
-          target: ref,
-          fromTo: [{ opacity: 1 }, { opacity: 0, ease: "none" }],
-        }}
-      />
-    </>
-  );
-}
+const Character = forwardRef<HTMLImageElement, { name: string }>(
+  function Character({ name }, ref) {
+    const className = `dancing-${name}`;
+    return (
+      <>
+        <img
+          ref={ref}
+          src={`/dance-${name}.png`}
+          alt={`Dancing ${name}`}
+          className={`${styles[className]} image-fixed ${styles["color-highlight"]} ${styles["start-hidden"]}`}
+        />
+        <img
+          src={`/dark-${name}.png`}
+          alt={`Dark dancing ${name}`}
+          className={`${styles[className]} image-fixed`}
+        />
+      </>
+    );
+  }
+);
 
 const pineappleSrc = (frame: number) => `/dance-pineapple-${frame}.png`;
 const danceFrames = 3;
@@ -118,16 +89,55 @@ function DancePineapple() {
   );
 }
 
+const characters = ["avocado", "strawberry", "orange", "pepper"];
+
 export default function DanceScene() {
+  const fadeTimeline = getStaggeredTimeline({
+    start: 0,
+    end: 100,
+    chunks: 20,
+    overlap: 0.4,
+  });
+  const avocadoRef = useRef<HTMLImageElement>(null);
+  const strawberryRef = useRef<HTMLImageElement>(null);
+  const orangeRef = useRef<HTMLImageElement>(null);
+  const pepperRef = useRef<HTMLImageElement>(null);
+
+  const refs = [avocadoRef, strawberryRef, orangeRef, pepperRef];
+
   return (
     <Root start="top top" end="bottom bottom" scrub={2}>
       <Pin childHeight={"100vh"} pinSpacerHeight={`800vh`}>
         <Panel className={styles["party-background"]}>
           <DancePineapple />
-          <FadeInAndOut name="strawberry" start={0} end={15} />
-          <FadeInAndOut name="avocado" start={15} end={30} />
-          <FadeInAndOut name="orange" start={30} end={45} />
-          <FadeInAndOut name="pepper" start={45} end={60} />
+          {fadeTimeline.map(({ start, end }, idx) => {
+            const remainder = idx % characters.length;
+            const ref = refs[remainder];
+            return (
+              <Animation
+                tween={{
+                  target: ref,
+                  start,
+                  end,
+                  fromTo: [
+                    {
+                      opacity: 0,
+                    },
+                    {
+                      keyframes: {
+                        "0%": { opacity: 0 },
+                        "50%": { opacity: 1 },
+                        "100%": { opacity: 0 },
+                      },
+                    },
+                  ],
+                }}
+              />
+            );
+          })}
+          {characters.map((name, idx) => {
+            return <Character name={name} ref={refs[idx]} />;
+          })}
         </Panel>
       </Pin>
     </Root>
