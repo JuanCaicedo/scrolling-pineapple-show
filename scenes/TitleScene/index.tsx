4d9e645844f35f535277b4816325e9deeb7ec7de
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-11-06 09:21:55 -0700

    animate show

--- scenes/TitleScene/index.tsx
+++ scenes/TitleScene/index.tsx
@@ -9,9 +9,20 @@ export default function TitleScene() {
   const scrollingBlockRef = useRef<HTMLDivElement>(null);
   const pineappleRef = useRef<HTMLDivElement>(null);
 
+  const showSRef = useRef<HTMLSpanElement>(null);
+  const showHRef = useRef<HTMLSpanElement>(null);
+  const showORef = useRef<HTMLSpanElement>(null);
+  const showWRef = useRef<HTMLSpanElement>(null);
+  const showRefs = [showSRef, showHRef, showORef, showWRef];
+
   const timeline = getStaggeredTimeline({
     start: 0,
     end: 40,
+    chunks: 4,
+  });
+  const showTimeline = getStaggeredTimeline({
+    start: timeline[3].start,
+    end: 60,
     chunks: 4,
   });
 
@@ -31,7 +42,32 @@ export default function TitleScene() {
         <div className={styles["pineapple"]} ref={pineappleRef}>
           Pineapple
         </div>
-        <div className={styles["show"]}>Show</div>
+        <div className={styles["show"]}>
+          <span className={styles["show-container"]}>
+            <span className={styles["see-through"]}>S</span>
+            <span className={styles["show-absolute"]} ref={showSRef}>
+              S
+            </span>
+          </span>
+          <span className={styles["show-container"]}>
+            <span className={styles["see-through"]}>h</span>
+            <span className={styles["show-absolute"]} ref={showHRef}>
+              h
+            </span>
+          </span>
+          <span className={styles["show-container"]}>
+            <span className={styles["see-through"]}>o</span>
+            <span className={styles["show-absolute"]} ref={showORef}>
+              o
+            </span>
+          </span>
+          <span className={styles["show-container"]}>
+            <span className={styles["see-through"]}>w</span>
+            <span className={styles["show-absolute"]} ref={showWRef}>
+              w
+            </span>
+          </span>
+        </div>
       </div>
       <Animation
         tween={{
@@ -80,6 +116,53 @@ export default function TitleScene() {
           ],
         }}
       />
+      {showTimeline.map((timeline, index) => {
+        return (
+          <>
+            <Animation
+              key={`show-${index}`}
+              tween={{
+                start: timeline.start,
+                end: timeline.end,
+                target: showRefs[index],
+                fromTo: [
+                  {
+                    opacity: 0,
+                  },
+                  {
+                    opacity: 1,
+                    ease: 'none'
+                  },
+                ],
+              }}
+            />
+            <Animation
+              key={`show-${index}-2`}
+              tween={{
+                start: timeline.start,
+                end: timeline.end,
+                target: showRefs[index],
+                to: {
+                  keyframes: {
+                    "0%": {
+                      transform: "translateY(0)",
+                    },
+                    "30%": {
+                      transform: "translateY(-3cqh)",
+                    },
+                    "60%": {
+                      transform: "translateY(0)",
+                    },
+                    "100%": {
+                      transform: "translateY(0)",
+                    },
+                  },
+                },
+              }}
+            />
+          </>
+        );
+      })}
     </Panel>
   );
 }
