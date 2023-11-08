28dc7835120d340348a391b02afca88d14ecf32c
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-11-08 08:39:01 -0700

    image updates

--- scenes/IpodScene/index.tsx
+++ scenes/IpodScene/index.tsx
@@ -15,10 +15,6 @@ import styles from "./index.module.css";
 import { getStaggeredTimeline } from "@/app/utils/getStaggeredTimeline";
 import Panel from "@/components/Panel";
 
-const totalFrames = 17;
-
-const frames = (frame: number): number => frame * (100 / totalFrames);
-
 const ipodFrames = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
 const spinSrc = (frame: number): string => `/ipod-${frame}.png`;
@@ -71,7 +67,7 @@ export default function IpodScene() {
           target: canvasRef,
           start: 0,
           end: lastTimeline.end,
-          fromTo: [{ top: "-20cqh" }, { top: "55cqh" }],
+          fromTo: [{ top: "0cqh" }, { top: "95cqh" }],
         }}
       />
       <div className={styles.container}>
