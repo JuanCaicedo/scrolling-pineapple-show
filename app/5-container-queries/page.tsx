50561bf289cfbf919fb229cd3dc31d7dc840479c
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-11-15 11:16:34 -0500

    animation for queries

--- app/5-container-queries/page.tsx
+++ app/5-container-queries/page.tsx
@@ -4,14 +4,34 @@ import { Pin, Root, Animation } from "@bsmnt/scrollytelling";
 import "./styles.css";
 
 export default function Panel() {
+  const ref = useRef<HTMLDivElement>(null);
   return (
     <Root>
       <Pin childHeight={"100vh"} pinSpacerHeight={"800vh"} top="0">
         <div className="demo-panel-container">
           <div className="demo-panel">
-            <img src="/pineapple-run-1.png" className="demo-image pineapple" />
-            <img src="/racoon-run-1.png" className="demo-image racoon" />
+            <div className="relative-container" ref={ref}>
+              <img
+                src="/pineapple-run-1.png"
+                className="demo-image pineapple"
+              />
+              <img src="/racoon-run-1.png" className="demo-image racoon" />
+            </div>
           </div>
+
+          <Animation
+            tween={{
+              target: ref,
+              start: 0,
+              end: 100,
+              fromTo: [
+                {
+                  left: "0px",
+                },
+                { left: "600px" },
+              ],
+            }}
+          />
         </div>
       </Pin>
     </Root>
