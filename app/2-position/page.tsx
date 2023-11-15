f1c559b090b0808528022f92397c133d6aa9da7f
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-11-15 11:08:46 -0500

    remove unneeded container

--- app/2-position/page.tsx
+++ app/2-position/page.tsx
@@ -4,9 +4,8 @@ import { Pin, Root, Animation } from "@bsmnt/scrollytelling";
 import "./styles.css";
 
 export default function Position() {
-  const containerRef = useRef<HTMLDivElement>(null);
   return (
-    <div className="absolute-container" ref={containerRef}>
+    <div className="absolute-container">
       <div className="relative-container">
         <img src="/pineapple-run-1.png" className="small-image pineapple" />
         <img src="/racoon-run-1.png" className="small-image racoon" />
