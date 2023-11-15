a3c3197e9118b997b88bcd63bbbd891f68ba7386
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-11-15 11:06:05 -0500

    animation together

--- app/3-animation/page.tsx
+++ app/3-animation/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useRef } from "react";
 import { Pin, Root, Animation } from "@bsmnt/scrollytelling";
+import "./styles.css";
 
 export default function AnimationDemo() {
   const imgRef = useRef<HTMLImageElement>(null);
@@ -8,7 +9,12 @@ export default function AnimationDemo() {
   return (
     <Root>
       <Pin childHeight={"100vh"} pinSpacerHeight={"800vh"} top="0">
-        <img ref={imgRef} src="/pineapple-run-1.png" className="demo-image" />
+        <div className="absolute-container" ref={imgRef}>
+          <div className="relative-container">
+            <img src="/pineapple-run-1.png" className="small-image pineapple" />
+            <img src="/racoon-run-1.png" className="small-image racoon" />
+          </div>
+        </div>
         <Animation
           tween={{
             target: imgRef,
@@ -16,7 +22,7 @@ export default function AnimationDemo() {
             end: 100,
             fromTo: [
               {
-                left: "0px",
+                left: "100px",
               },
               { left: "600px" },
             ],
