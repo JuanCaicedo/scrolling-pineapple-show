cf6ee0c80e1f52024e26eb69fcded04dab735bd2
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-11-15 12:14:06 -0500

    tweaks panel

--- app/4-panel/page.tsx
+++ app/4-panel/page.tsx
@@ -4,15 +4,38 @@ import { Pin, Root, Animation } from "@bsmnt/scrollytelling";
 import "./styles.css";
 
 export default function Panel() {
+  const ref = useRef(null);
+
   return (
     <Root>
       <Pin childHeight={"100vh"} pinSpacerHeight={"800vh"} top="0">
         <div className="demo-panel-container">
           <div className="demo-panel">
-            <img src="/pineapple-run-1.png" className="demo-image pineapple" />
-            <img src="/racoon-run-1.png" className="demo-image racoon" />
+            <div className="absolute-container" ref={ref}>
+              <div className="relative-container">
+                <img
+                  src="/pineapple-run-1.png"
+                  className="demo-image pineapple"
+                />
+                <img src="/racoon-run-1.png" className="demo-image racoon" />
+              </div>
+            </div>
           </div>
         </div>
+
+        <Animation
+          tween={{
+            target: ref,
+            start: 0,
+            end: 100,
+            fromTo: [
+              {
+                left: "0px",
+              },
+              { left: "600px" },
+            ],
+          }}
+        />
       </Pin>
     </Root>
   );
