eefbdbefe7536618f7852cf671e94655aa92c5e7
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-11-16 18:39:32 -0500

    add ref for demo

--- app/3-animation/page.tsx
+++ app/3-animation/page.tsx
@@ -4,6 +4,7 @@ import { Pin, Root, Animation } from "@bsmnt/scrollytelling";
 import "./styles.css";
 
 export default function AnimationDemo() {
+  const ref = useRef(null);
   return (
     <div className="absolute-container">
       <div className="relative-container">
