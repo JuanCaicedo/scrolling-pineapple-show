21c3b655fd06309e73ce149e0899f066779c32d5
Juan Caicedo <retiredcanadianpoet@gmail.com>
2023-11-17 06:44:24 -0500

    access undefined

--- app/1-use-ref/page.tsx
+++ app/1-use-ref/page.tsx
@@ -5,8 +5,8 @@ export default function UseRef() {
   const ref = useRef(null);
 
   useEffect(() => {
-    console.log("ref.current.width", ref.current.width);
-    console.log("ref.current.height", ref.current.height);
+    console.log("ref.current.width", ref?.current?.width);
+    console.log("ref.current.height", ref?.current?.height);
   }, []);
 
   return <img src="/pineapple-run-1.png" className="demo-image" ref={ref} />;
